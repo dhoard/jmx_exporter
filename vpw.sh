c351569662b62d9a3258092ee05a3f269eb527a0
dhoard <doug.hoard@gmail.com>
2024-11-10 15:52:18 -0500

    Updated vpw.sh
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- vpw.sh
+++ vpw.sh
@@ -5,17 +5,23 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Set the default JAR path
 JAR_PATH="target/verifyica-pipeline-0.0.1.jar"
 
-# Check if the JAR exists in the target directory; if not, assume it's in the root directory
 if [ ! -f "$JAR_PATH" ]; then
     JAR_PATH="verifyica-pipeline-0.0.1.jar"
 fi
 
-# Check if the JAR exists in the target directory; if not, assume it's in the root directory
 if [ ! -f "$JAR_PATH" ]; then
      JAR_PATH=".verifyica/verifyica-pipeline-0.0.1.jar"
+fi
+
+if [ ! -f "$JAR_PATH" ]; then
+     JAR_PATH=".verifyica/verifyica-pipeline-0.0.1.jar"
+fi
+
+if [ ! -f "$JAR_PATH" ]; then
+    echo "verifyica-pipeline-0.0.1.jar not found"
+    exit 1
 fi
 
 java -jar "${JAR_PATH}" "$@"
